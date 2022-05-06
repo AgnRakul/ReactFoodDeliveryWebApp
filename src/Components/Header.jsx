4e9356603ad13ed5bf9516edaeac3f4dae25c2fc
RakulAgn <rakul0agn@gmail.com>
2022-05-06 12:02:52 +0530

    Setup Push

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="px-16 fixed z-50 w-screen bg-slate-300 p-6">
+      <div className="hidden md:flex w-full h-full"></div>
+
+      <div className="flex md:hidden w-full h-full "></div>
+    </div>
+  );
+};
+
+export default Header;
