ffe990cee4f385e43b03c5b71fe1d913c62fd107
RakulAgn <rakul0agn@gmail.com>
2022-05-09 11:01:50 +0530

    added Card Layers

--- src/Components/HomeContainer.jsx
+++ src/Components/HomeContainer.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Delivery from "../Images/delivery.png";
 import HeroBg from "../Images/heroBg.png";
+import I1 from "../Images/i1.png";
 
 const HomeContainer = () => {
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full" id="home">
-      <div className="py-2 flex-1 flex flex-col items-start  justify-center gap-6">
+    <section
+      className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full "
+      id="home"
+    >
+      <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
         <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
           <p className="text-base text-orange-500 font-semibold">
             Bike Delivery
@@ -21,33 +25,43 @@ const HomeContainer = () => {
 
         <p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
           The Fastest Delivery in
-          <span className="text-orange-600 md:text-[5rem] text-[3rem]">
+          <span className="text-orange-600 text-[3rem] lg:text-[5rem]">
             Your City
           </span>
         </p>
+
         <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima velit
+          eaque fugit distinctio est nam voluptatum architecto, porro iusto
+          deserunt recusandae ipsa minus eos sunt, dolores illo repellat facere
+          suscipit!
         </p>
+
         <button
           type="button"
-          className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
+          className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2  rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
         >
           Order Now
         </button>
       </div>
-      <div className="py-2 flex-1 flex items-center">
-        <div className="w-full flex item-center justify-center relative">
-          <img
-            src={HeroBg}
-            className="ml-auto w-full lg:w-auto lg:h-650 h-420"
-            alt="hero-bg"
-          />
-          <div className="w-full h-full absolute flex item-center justify-center"></div>
+      <div className="py-2 flex-1 flex items-center relative">
+        <img
+          src={HeroBg}
+          className=" ml-auto h-420 w-full lg:w-auto lg:h-650"
+          alt="hero-bg"
+        />
+
+        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32  py-4 gap-4 flex-wrap">
+          <div className="w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex items-center justify-center flex-col ">
+            <img src={I1} alt="I1" className="w-40 -mt-20" />
+            <p className="text-lg font-semibold text-textColor">IceCream</p>
+            <p className="text-md text-lighttextGray font-semibold my-3">
+              Chocolate Vanilla
+            </p>
+            <p className="text-sm font-semibold text-headingColor">
+              <span className="text-xs text-red-600">$</span> 5.25
+            </p>
+          </div>
         </div>
       </div>
     </section>
